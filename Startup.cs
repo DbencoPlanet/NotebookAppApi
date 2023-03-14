2cb03068a5c16fd5f03af793005e2a40128d7775
Bernard Amaeme <bernardamaeme@gmail.com>
2023-03-14 22:57:22 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using NotebookAppApi.Data;
+using NotebookAppApi.Interfaces;
+using NotebookAppApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotebookAppApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Add service and create Policy with options
+
+            services.AddCors();
+           
+           //services.AddCors(options => {
+           //    options.AddPolicy("CorsPolicy",
+           //                                builder => builder.AllowAnyOrigin()
+           //                                                  .AllowAnyMethod()
+           //                                                  .AllowAnyHeader()
+           //                                                  .AllowCredentials());
+           //});
+           // ....
+           // Add framework services.
+           services.AddMvc();
+            services.Configure<Settings>(options =>
+            {
+                options.ConnectionString
+                    = Configuration.GetSection("MongoConnection:ConnectionString").Value;
+                options.Database
+                    = Configuration.GetSection("MongoConnection:Database").Value;
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NotebookAppApi", Version = "v1" });
+            });
+
+            services.AddTransient<INoteRepository, NoteRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.UseCors("CorsPolicy");
+            app.UseCors();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NotebookAppApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
